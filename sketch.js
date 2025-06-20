b7e38045a6b16529026f6dc835c2c73f50af4e1d
Lyra7770 <lyramir955@gmail.com>
2025-06-20 17:40:29 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,19 +2,19 @@ let trazos = [];
 let trazoActual = null;
 
 const paleta = [
-  [222, 218, 145],
-  [191, 186, 176],
-  [166, 136, 99],
-  [198, 153, 144],
-  [144, 178, 126],
-  [238, 227, 202]
+  [222, 218, 145], // Amarillo suave
+  [191, 186, 176], // Gris cálido
+  [166, 136, 99],  // Ocre
+  [198, 153, 144], // Rosado viejo
+  [144, 178, 126], // Verde apagado
+  [238, 227, 202]  // Fondo cálido claro
 ];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(...paleta[5]);
-  textFont('Cormorant Garamond');
-  textSize(70);
+  textFont('Satoshi');
+  textSize(64);
   textAlign(CENTER, TOP);
   smooth();
 }
@@ -22,9 +22,10 @@ function setup() {
 function draw() {
   background(...paleta[5]);
 
+  // Título limpio, moderno y elegante
   fill(30, 30, 30, 140);
   noStroke();
-  text("Dibuja un recuerdo", width / 2, height * 0.035);
+  text("Dibuja un recuerdo", width / 2, height * 0.04);
 
   for (let t of trazos) {
     t.mostrar();
@@ -34,6 +35,7 @@ function draw() {
     trazoActual.mostrar();
   }
 
+  // Cursor suave personalizado
   noCursor();
   fill(30, 30, 30, 60);
   ellipse(mouseX, mouseY, 20, 20);
